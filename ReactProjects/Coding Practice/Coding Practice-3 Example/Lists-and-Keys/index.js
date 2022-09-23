13c44f39830ced0e476ae21eb1cb1e7d9476f777
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2022-09-23 15:33:02 +0530

    Add files via upload

--- ReactProjects/Coding Practice/Coding Practice-3 Example/Lists-and-Keys/index.js
+++ ReactProjects/Coding Practice/Coding Practice-3 Example/Lists-and-Keys/index.js
@@ -1,17 +1,10 @@
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-const UserProfile = props => {
-  const {userDetails} = props
-  const {imageUrl, name, role} = userDetails
-
-  return (
-    <li className="user-card-container">
-      <img src={imageUrl} className="avatar" alt="avatar" />
-      <div className="user-details-container">
-        <h1 className="user-name"> {name} </h1>
-        <p className="user-designation"> {role} </p>
-      </div>
-    </li>
-  )
-}
-export default UserProfile
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
