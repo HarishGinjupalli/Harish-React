fa098ebd72f7574f325d696f6d10b6c904fec93b
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2023-04-14 15:13:05 +0530

    Add files via upload

--- NxtWave/demo/Coding Practice-1/1) Super Over League/Component/SuperOverLeague.jsx
+++ NxtWave/demo/Coding Practice-1/1) Super Over League/Component/SuperOverLeague.jsx
@@ -1,1 +1,25 @@
+import React from "react";
+import "./superoverleague.css"
+import csk from "../Images/csk-img.jpg"
+import rcb from "../Images/rcb-img.jpg"
 
+class SuperOverLeague extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div className="bg-container d-flex flex-column justify-content-center">
+                    <h1 className="heading text-center">Super Over League</h1>
+                    <div className="container">
+                        <div className="d-flex flex-row justify-content-center">
+                            <img className="image" src={rcb}></img>
+                            <img className="image" src={csk}></img>
+                        </div>
+
+                    </div>    
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default SuperOverLeague
