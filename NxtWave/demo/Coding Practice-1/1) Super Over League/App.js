81193c3c269b8e764200718c28f0cdbc14808120
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2023-04-14 15:15:27 +0530

    Add files via upload

---
+++ NxtWave/demo/Coding Practice-1/1) Super Over League/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import SuperOverLeague from "./Superoverleague/SuperOverLeague";
+
+function App() {
+
+
+    return (
+
+        <React.Fragment>
+            {/* <Navbar></Navbar> */}
+
+            <SuperOverLeague></SuperOverLeague>
+        </React.Fragment>
+
+    );
+}
+
+export default App;
